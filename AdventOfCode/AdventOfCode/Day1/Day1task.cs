9de9281aa71ae1511ab0f35bc76b142d29676dd5
Kacper <kacper.szczepanek@nordcloud.com>
2019-12-06 10:27:35 +0100

    day1.2

--- AdventOfCode/AdventOfCode/Day1/Day1task.cs
+++ AdventOfCode/AdventOfCode/Day1/Day1task.cs
@@ -6,11 +6,10 @@ namespace AdventOfCode.Day1
         public Day1task()
         {
 
-            string[] lines = System.IO.File.ReadAllLines(@"/Users/Kacper/Documents/Repos/AdventOfCode/AdventOfCode/AdventOfCode/Day1/input.txt");
+            string[] lines = System.IO.File.ReadAllLines(@"/Users/kacper.szczepanek/Documents/AdventOfCode/AdventOfCode/AdventOfCode/Day1/input.txt");
             int[] input = new int[lines.Length];
             int i = 0;
             int sum = 0;
-            int negativeSum = 0;
 
             foreach (var item in lines) //Change all strings from text file to ints in array input
             {
@@ -20,17 +19,24 @@ namespace AdventOfCode.Day1
 
             foreach (var item in input)
             {
-                sum += SantasFuellCalculate(item);
+                sum += SantaFuelCalculateAll(item);
             }
 
             Console.WriteLine(sum);
 
-            Console.WriteLine(NegativeFuelCopy(SantasFuellCalculate(14)));
-            Console.WriteLine(NegativeFuelCopy(SantasFuellCalculate(21)));
-            Console.WriteLine(SantasFuellCalculate(1969) + NegativeFuelCopy(SantasFuellCalculate(1969)));
-            Console.WriteLine(SantasFuellCalculate(100756) + NegativeFuelCopy(SantasFuellCalculate(100756)));
+        }
 
+        public int SantaFuelCalculateAll(int mass)
+        {
+            int sumFuel = 0;
+            while (mass > 0)
+            {
+                int r = SantasFuellCalculate(mass);
+                sumFuel += r;
+                mass = r;
+            }
 
+            return sumFuel;
         }
 
         public int SantasFuellCalculate(int mass)
@@ -40,70 +46,7 @@ namespace AdventOfCode.Day1
             fuellNeeded = mass / 3;
             fuellNeeded -= 2;
 
-            return fuellNeeded;
-        }
-
-        public int NegativeFuell(int mass)
-        {
-            int fuellNeeded;
-
-            fuellNeeded = mass / 3;
-            if (fuellNeeded > 0)
-            {
-                if (fuellNeeded - 2 > 2)
-                {
-                    fuellNeeded -= 2;
-                    return fuellNeeded;
-                }
-                else
-                {
-                    return fuellNeeded;
-                }
-            }
-            else
-            {
-                return fuellNeeded;
-            }
-        }
-
-        public int NegativeFuelCopy(int mass)
-        {
-            int fuellNeeded;
-            int fuellSubSum = 0;
-
-            while (mass != 0)
-            {
-                if (mass / 3 != 1)
-                {
-                    fuellNeeded = mass / 3;
-                    if (fuellNeeded > 0)
-                    {
-                        if (fuellNeeded - 2 > 0)
-                        {
-                            fuellNeeded -= 2;
-                            fuellSubSum += fuellNeeded;
-                            mass = fuellNeeded;
-                        }
-                        else
-                        {
-                            fuellSubSum += fuellNeeded;
-                            mass = fuellNeeded;
-                        }
-                    }
-                    else
-                    {
-                        fuellSubSum += fuellNeeded;
-                        mass = fuellNeeded;
-                    }
-                }
-                else
-                {
-                    fuellSubSum += mass;
-                    break;
-                }
-            }
-
-            return fuellSubSum;
+            return Math.Max(fuellNeeded, 0);
         }
     }
 }
